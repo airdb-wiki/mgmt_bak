ecb61c8844574e5618e7db5eee75ac06df129deb
IWTF <hzw2658177@163.com>
2018-10-02 16:14:20 +0800

    .

--- src/pages/detail/index.vue
+++ src/pages/detail/index.vue
@@ -41,22 +41,21 @@ export default{
       const ctx = wx.createCanvasContext('myCanvas')
       // 填充背景色
       ctx.fillStyle = '#fff'
-      ctx.fillRect(0, 0, 300, 600)
+      ctx.fillRect(0, 0, 260, 960)
 
-      ctx.setFontSize(28)
+      ctx.setFontSize(20)
       ctx.setFillStyle('#393939')
-      ctx.fillText('文字在这里！！！', 40, 20)
-      ctx.fillText('dean', 10, 6)
+      ctx.fillText('文字在这里！！！', 20, 5, 800)
+      ctx.setTextAlign('center')
 
       ctx.font = 'italic bold 20px cursive'
       const metrics = ctx.measureText('Hello World')
       console.log(metrics.width)
 
-      ctx.drawImage('/static/images/mina/8.jpg', 40, 40, 60, 60)
+      ctx.drawImage('/static/images/mina/8.jpg', 20, 5, 120, 60)
 
-      wx.showToast({
+      wx.showLoading({
         title: '分享图片生成中...',
-        duration: 1000,
         mask: true
       })
 
@@ -65,13 +64,14 @@ export default{
         wx.canvasToTempFilePath({
           x: 0,
           y: 0,
-          width: 300,
-          height: 600,
-          destWidth: 300,
-          destHeight: 600,
+          width: 780,
+          height: 800,
+          destWidth: 780,
+          destHeight: 800,
           canvasId: 'myCanvas',
           fileType: 'png',
           success: function (res) {
+            wx.hideLoading()
             wx.previewImage({
               urls: [res.tempFilePath]
             })
