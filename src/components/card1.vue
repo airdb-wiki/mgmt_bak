7cf351d06709eb1aae1953617318062d65e7f681
IWTF <hzw2658177@163.com>
2018-10-20 08:34:13 +0800

    home页修改

--- src/components/card1.vue
+++ src/components/card1.vue
@@ -6,21 +6,20 @@
           <div class="weui-cell__hd">
             <image :src="item.AvatarUrl" style="vertical-align: middle;width:65px; height: 100px;border-radius: 5px 0 0 5px;"></image>
           </div>
-          <div class="info_container">
+          <div class="weui-cell__bd" style="margin-left: 8px;">
             <div class="title">{{item.title}}寻找回家的路，让你不再孤立无援</div>
             <div class="info_1">
-              <text>{{item.Age}}岁</text>
-              <text>{{item.sex}}男</text>
-            </div>
-            <div class="address">
-              <img src="/static/images/home/position.png" style="width: 18px;height: 18px;">
+              <text v-if="item.Gender == 2">女</text>
+              <text v-else>男</text>
+              <img src="/static/images/home/position.png" style="width: 16px;height: 16px;">
               <text>{{item.MissedAddress}}河南省洛阳市</text>
             </div>
-            <div class="inf0_2">跟进志愿者：{{item.editor}}南舒北朗</div>
-            <div>注册时间：{{item.time}}2017-6-19</div>
+            <div class="pub_info">
+              <div>{{item.Nickname}}发表于宝贝回家</div>
+                <img src="/static/images/home/notice.png">
+                <text style="position: absolute; right: 10px;line-height: 20px;">2000</text>
+            </div>
           </div>
-          <!-- <div class="weui-cell__bd">标题文字</div>
-          <div class="weui-cell__ft">说明文字</div> -->
         </div>
       </div>
     </scroll-view>
@@ -35,16 +34,20 @@ export default {
 </script>
 
 <style scoped>
-.info_container{
-  display: flex;
-  flex-direction: column;
-  flex: 3;
-  padding-left: 10px;
-  line-height: 20px;
-  background: rgba(255, 99, 7, .2);
-}
 .address{
   margin-left: 5px;
+}
+.pub_info{
+  display: flex;
+  flex-direction: row;
+  position: relative;
+  font-size: 14px;
+}
+.pub_info image{
+  width: 20px;
+  height: 20px;
+  position: absolute;
+  right: 45px;
 }
 .title{
   font-weight: bold;
@@ -52,13 +55,19 @@ export default {
   font-size: 18px;
 }
 .info_1 text{
+  padding: 0px 10px;
+  border-radius: 5px;
+  background: #e2e2e2;
   margin-right: 20px;
+  font-size: 14px;
 }
 .weui-cell{
   padding: 0;
   margin: 6px 10px;
   border: none;
+  line-height: 28px;
   border-radius: 5px;
   box-shadow: 0 0 5px #b1b1b1;
+  background: rgba(255, 99, 7, .2);
 }
 </style>
