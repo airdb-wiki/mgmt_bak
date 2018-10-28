e577b19b72ffad012df81f55a4c2bd436c556006
IWTF <hzw2658177@163.com>
2018-10-28 10:23:58 +0800

    trend

--- src/pages/test_mc/trends/index.vue
+++ src/pages/test_mc/trends/index.vue
@@ -1,26 +1,63 @@
 <template>
-  <div class>
+  <div>
     <navigation :search='false'></navigation>
 
     <!-- 顶部背景 -->
     <div class="trans" :style="style"></div>
     <image src="/static/images/home/sls.png" mode="scaleToFill" class="bg"></image>
 
-    <!-- navBar -->
-    <div class="bar">
-      <scroll-view :scroll-x="true" class="navbar">
-        <div v-for="tab in tabs" :key="tab.id" class="tab">{{tab}}</div>
-      </scroll-view>
-    </div>
+    <div class="more">+</div>
     
-    <div class="weui-cells weui-cells_after-title">
-        <div class="weui-cell" v-for="item in num" :key="item.id" @click="navTo">
-          <div class="weui-cell__hd">
-            <image :src="icon" style="margin-right: 5px;vertical-align: middle;width:20px; height: 20px;"></image>
-          </div>
-          <div class="weui-cell__bd">标题文字</div>
-          <div class="weui-cell__ft">说明文字</div>
+    <div class="trend_container">
+      <div style="img">
+        <image src="/static/images/home/wx.png" style="width: 50px;height: 50px;"></image>
+      </div>
+      <div class="content">
+        <div style="font-size: 18px;font-weight: bold;">仙人球</div>
+        <div>{{content}}</div>
+        <div class="imgContainer">
+          <image class="weui-article__img" src="/static/images/home/sls.png" mode="aspectFill"/>
+          <image class="weui-article__img" src="/static/images/home/sls.png" mode="aspectFill"/>
         </div>
+        <div class="talk">
+          <div>{{time}}</div>
+          <image src="/static/images/home/talk1.png"></image>
+        </div>
+      </div>
+    </div>
+    <div class="trend_container">
+      <div style="img">
+        <image src="/static/images/home/wx.png" style="width: 50px;height: 50px;"></image>
+      </div>
+      <div class="content">
+        <div style="font-size: 18px;font-weight: bold;">仙人球</div>
+        <div>{{content}}</div>
+        <div class="imgContainer">
+          <image class="weui-article__img" src="/static/images/home/sls.png" mode="aspectFill"/>
+          <image class="weui-article__img" src="/static/images/home/sls.png" mode="aspectFill"/>
+        </div>
+        <div class="talk">
+          <div>{{time}}</div>
+          <image src="/static/images/home/talk1.png"></image>
+        </div>
+      </div>
+    </div>
+    <div class="trend_container">
+      <div style="img">
+        <image src="/static/images/home/wx.png" style="width: 50px;height: 50px;"></image>
+      </div>
+      <div class="content">
+        <div style="font-size: 18px;font-weight: bold;">仙人球</div>
+        <div>{{content}}</div>
+        <div class="imgContainer">
+          <image class="weui-article__img" src="/static/images/home/sls.png" mode="aspectFill"/>
+          <image class="weui-article__img" src="/static/images/home/sls.png" mode="aspectFill"/>
+        </div>
+        <div class="talk">
+          <div>{{time}}</div>
+          <image src="/static/images/home/talk1.png"></image>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -40,7 +77,9 @@ export default {
       num: [
         '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'
       ],
-      style: 'background: rgba(255, 255, 255, 0)'
+      style: 'background: rgba(255, 255, 255, 0)',
+      content: '2014年6月8日，经人介绍，约32岁的陈连发在宝贝回家寻子网站进行了寻亲登记，寻家工作组志愿者精彩跟进该案例，及时和陈连发进行沟通',
+      time: '一天前'
     }
   },
   onShareAppMessage: function () {
@@ -51,7 +90,7 @@ export default {
     }
   },
   onPageScroll (res) {
-    this.style = 'background: rgba(255, 255, 255, ' + res.scrollTop / 120 + ')'
+    this.style = 'background: rgba(255, 255, 255, ' + res.scrollTop / 150 + ')'
   },
   methods: {
     navTo () {
@@ -64,13 +103,13 @@ export default {
 <style scoped>
 .trans{
     width: 100%;
-    height: 130px;
+    height: 180px;
     margin-top: 72px;
     background-color: rgba(255, 255, 255, 0);
 }
 .bg{
   width: 100%;
-  height: 130px;
+  height: 180px;
   position: fixed;
   top: 72px;
   left: 0;
@@ -121,5 +160,59 @@ export default {
   position: absolute;
   top: 0;
   right: 0;
+}
+.trend_container{
+  display: flex;
+  position: relative;
+  flex-direction: row;
+  width: 100%;
+  padding: 10px;
+  padding-bottom: 5px;
+  background-color: #fff;
+  border-bottom: 1px solid #cccccc;
+}
+.img{
+  width: 50px;
+  height: 50px;
+}
+.content{
+  padding: 0px 40px 10px 10px;
+}
+.imgContainer{
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+.imgContainer image{
+  width: 80px;
+  height: 80px;
+  margin: 2px;
+}
+.talk{
+  position: relative;
+  display: flex;
+  padding-top: 5px;
+}
+.talk image{
+  width: 20px;
+  height: 20px;
+  padding-top: 5px;
+  position: absolute;
+  top: 5px;
+  right: 0;
+}
+.more{
+  position: fixed;
+  bottom: 30px;
+  right: 30px;
+  z-index: 999;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  color: #fff;
+  background-color: #1296db;
+  font-size: 35px;
+  text-align: center;
+  line-height: 45px;
 }
 </style>
