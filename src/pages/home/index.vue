56cddc573444494f5dc0122e9862fc9fca99dfa4
IWTF <hzw2658177@163.com>
2018-11-04 21:01:32 +0800

    a

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -284,33 +284,7 @@ export default {
   padding: 2px 0;
   position: relative;
 }
-
-/* 假的空图层 */
-.navbar1{
-  width: 100%;
-  white-space: nowrap;
-  position: fixed;
-  top: 53pt;
-  left: 0;
-  z-index: 999;
-  background-color: transparent;
-  overflow: hidden;
-}
-::scrollbar {
-  width: 0;
-  height: 0;
-  color: transparent;
-}
-.tab1{
-  text-align: center;
-  font-size: 20px;
-  width: 100px;
-  display: inline-block;
-  box-sizing: border-box;
-  padding: 2px 0;
-  position: relative;
-}
-.tab1:hover{
+.tab:hover{
   color: black;
   font-weight: bold;
   text-align: center;
@@ -318,14 +292,11 @@ export default {
   width: 100px;
   display: inline-block;
   box-sizing: border-box;
-  background-image: url('https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1541312159946&di=698558c0ce1040cedf233c39b0d1841e&imgtype=0&src=http%3A%2F%2Fpic38.photophoto.cn%2F20160311%2F0021033817789866_b.jpg');
+  background-image: linear-gradient(to right, transparent 1%, rgba(255, 255, 255, .95) 99%), url('https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1541312159946&di=698558c0ce1040cedf233c39b0d1841e&imgtype=0&src=http%3A%2F%2Fpic38.photophoto.cn%2F20160311%2F0021033817789866_b.jpg');
   background-size: 140px 130px;
   background-position: -20px 82px;
   background-repeat: repeat;
-  opacity: .3;
 }
-
-
 .mySwiper{
   margin: auto;
   margin-top: 107px;
@@ -339,16 +310,15 @@ export default {
   margin-left: 10px;
 }
 .bar::after{
-  content: '+';
-  font-size: 20px;
-  font-weight: bold;
+  content: 'ab';
+  color: transparent;
   line-height: 28px;
   position: fixed;
-  background-color: rgba(255, 255, 255, .85);
+  background: linear-gradient(to right, transparent 20%, #fff 80%);
   z-index: 9999;
   height: 30px;
   padding: 0 7px;
   top: 75px;
-  right: 0;
+  right: 0px;
 }
 </style>
