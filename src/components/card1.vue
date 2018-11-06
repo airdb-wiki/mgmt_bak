aede75e073866d738fb0cec57ea468f9b42ba3b3
IWTF <hzw2658177@163.com>
2018-11-06 22:41:42 +0800

    重新编译

--- src/components/card1.vue
+++ src/components/card1.vue
@@ -29,7 +29,7 @@
               </navigator> -->
 
               <!-- 点击地址时收集 -->
-              <div class="addr-wrapper" @click="formSubmit_addr" :id="item.UUID">
+              <div class="addr-wrapper" @click.stop="formSubmit_addr" :id="item.UUID">
                 <img src="/static/images/home/position.png" style="width: 20px;height: 20px;">
                 <div class="text-addr">
                   {{item.MissedAddress}}
