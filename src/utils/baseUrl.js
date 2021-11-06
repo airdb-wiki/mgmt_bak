2dcba20812ca6882c806d5ba588e57e91b233c6e
xujun <xujun@pingpongx.com>
2021-11-06 20:24:25 +0800

    feat: + 根据小程序运行环境改变baseUrl

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,21 +1,20 @@
-let BASE_URL = "https://scf.baobeihuijia.com"
+import Taro from '@tarojs/taro'
+const BASE_URL = "https://scf.baobeihuijia.com"
 
 const getBaseUrl = (url) => {
-    // let BASE_URL = '';
     let domain = BASE_URL
-    if (process.env.NODE_ENV === 'development') {
-        domain += "/test"
-            //开发环境 - 根据请求不同返回不同的BASE_URL
-            // if (url.includes('/api/')) {
-            //    BASE_URL = ''
-            // } else if (url.includes('/iatadatabase/')) {
-            //    BASE_URL = ''
-            // }
-    } else {
-        // 生产环境
-        domain += "/test"
+    const system = Taro.getAccountInfoSync()
+    switch(system.miniProgram.envVersion) {
+        case 'develop':
+            domain += "/test"
+            break;
+        case 'trial':
+            domain += "/test"
+            break;
+        case 'release':
+            domain += "/test"
+            break;
     }
-
     return domain
 }
 
