3eca7f23553641e4e989ee879016d5df24e267b6
jerry-shao <com.jerryshao@jerryshao.com>
2021-11-10 10:54:37 +0000

    Update style of Contact component

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -18,7 +18,7 @@ export default defineComponent({
   methods: {
     handleOpenCustomerServiceChat() {
       Taro.openCustomerServiceChat({
-        extInfo: { url: "https://work.weixin.qq.com/kfid/kfc02343d9ba414880a" },
+        extInfo: { url: "https://work.weixin.qq.com/kfid/kfc5fdb2e0a1f297753?sence=mp-bbhj" },
         corpId: "wx4aaa3fe59423b402",
         success(res) {
           console.log("xx");
