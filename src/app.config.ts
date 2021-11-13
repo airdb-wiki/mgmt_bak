0f73dfe74ae3679361489c57df8bb00def55241c
影子 <guozhigq@outlook.com>
2021-11-13 12:35:54 +0800

    fix:pageRoute

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,12 +1,20 @@
+/*
+ * @Author: your name
+ * @Date: 2021-11-13 11:01:17
+ * @LastEditTime: 2021-11-13 11:56:04
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /mp-bbhj/src/app.config.ts
+ */
 export default {
   pages: [
     'pages/index/index',
     'pages/rescue/index',
-    'pages/me/index',
     'pages/article/detail/index',
-    'pages/mine/thank/index',
-    'pages/mine/history/index',
-    'pages/mine/follow/index'
+    'pages/me/index/index',
+    'pages/me/thank/index',
+    'pages/me/history/index',
+    'pages/me/follow/index'
   ],
   window: {
     backgroundTextStyle: 'light',
@@ -22,7 +30,7 @@ export default {
       "pagePath": "pages/rescue/index",
       "text": "救助站"
     }, {
-      "pagePath": "pages/me/index",
+      "pagePath": "pages/me/index/index",
       "text": "我的"
     }]
   },
